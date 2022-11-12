0e2ee247c491819588e1a1de7f0d0bc3add741c8
İbrahim Aydın <aydnibrahim22@gmail.com>
2022-11-12 16:50:00 +0300

    Add project files.

---
+++ CustomerTracking/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerTracking
+{
+    internal class CustomerManager
+    {
+
+        
+        public static void Add(int customerId, string customerFirstName, string customerLastName) { 
+            var result = "Müşteri Eklendi : " + customerId + "-" + customerFirstName + " " + customerLastName;
+            Console.WriteLine(result);
+        }   
+
+        public static void Delete(int customerId, string customerFirstName, string customerLastName)
+        {
+            var result = "Müşteri bilgileri silindi : "+customerId+ "-" + customerFirstName + " " +customerLastName;
+            Console.WriteLine(result);
+        }
+
+        public static void Update(int customerId, string customerFirstName, string customerLastName, int ordersCount) {
+
+            var result = "Müşteri bilgileri güncellendi : " + customerId + "-" + customerFirstName + " " + customerLastName + "-" + ordersCount;
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ CustomerTracking/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+
+namespace CustomerTracking
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            CustomerManager.Add(1,"İbrahim Hakkı","Aydın");
+            CustomerManager.Update(1,"İbrahim Hakkı","Aydın",6);
+            CustomerManager.Delete(1,"İbrahim Hakkı","Aydın");
+        }
+
+    }
+        
+    
+}
